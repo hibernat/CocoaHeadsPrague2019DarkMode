3c3530fc12c3f0f8ed184125e8b96602984109ed
Michael Bernat <michael@hibernat.com>
2019-09-12 13:41:27 +0200

    UIImageView

--- DarkModeDemo/ViewController.swift
+++ DarkModeDemo/ViewController.swift
@@ -11,20 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     // ---------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .myStandardBackground
-        
         let myRGB = MyRGB(red: 0xFF, green: 0x58, blue: 0x57)
         self.label.textColor = UIColor(myRGB: myRGB)
-        
         self.label.backgroundColor = .myLabelBackground
-        
-        let gradientView = GradientView(frame: CGRect(x: 20, y: 150, width: 280, height: 400))
+        let gradientView = GradientView(frame: CGRect(x: 20, y: 400, width: 280, height: 100))
         self.view.addSubview(gradientView)
+        
+        self.imageView.image = UIImage(named: "myImage")
     }
 
 
---
+++ README.md
@@ -0,0 +1,2 @@
+# CocoaHeadsPrague2019DarkMode
+iOS 13 dark mode demo for Cocoa Heads Prague meetup on 12. September 2019
