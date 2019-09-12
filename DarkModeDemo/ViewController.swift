b6f953d2975f0304f5409bfef2d53a460e1ff553
Michael Bernat <michael@hibernat.com>
2019-09-12 15:23:05 +0200

    elevation - userInterfaceLevel

--- DarkModeDemo/ViewController.swift
+++ DarkModeDemo/ViewController.swift
@@ -9,29 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var button: UIButton!
     
     // ---------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .myStandardBackground
-        let myRGB = MyRGB(red: 0xFF, green: 0x58, blue: 0x57)
-        self.label.textColor = UIColor(myRGB: myRGB)
-        self.label.backgroundColor = .myLabelBackground
-        let gradientView = GradientView(frame: CGRect(x: 20, y: 400, width: 280, height: 100))
-        self.view.addSubview(gradientView)
-        
-        if #available(iOS 12, *) {
-            // this does not work, I consider it as a bug
-            self.imageView.image = UIImage(named: "myImage")?.imageAsset?.image(with: UITraitCollection(userInterfaceStyle: .dark))
-        } else {
-             self.imageView.image = UIImage(named: "myImage")
+        if #available(iOS 13, *) {
+            self.view.backgroundColor = .systemBackground
         }
     }
 
-
+    @IBAction func onButtonTouchUpInside(_ sender: Any) {
+        
+        let newViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! ViewController
+        self.present(newViewController, animated: true)
+    }
+    
 }
 
