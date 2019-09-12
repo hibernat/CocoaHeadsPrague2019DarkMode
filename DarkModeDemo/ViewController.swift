55c97fba0810e9b2da7013ea33b68de766580c5e
Michael Bernat <michael@hibernat.com>
2019-09-12 09:47:34 +0200

    Initial Commit

---
+++ DarkModeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DarkModeDemo
+//
+//  Created by Michael Bernat on 12/09/2019.
+//  Copyright © 2019 Michael Bernat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
